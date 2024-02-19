1dbdc66219f0361718f203c46c3ffe757ddac866
AquaroTorres <alvarotorres@gmail.com>
2024-02-18 22:11:43 -0300

    KillBill

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    globDirectory: './',
-    globPatterns: [
-        '**/*.{js,html,css}'
-    ],
-    swDest: 'sw.js',
-    ignoreURLParametersMatching: [
-        /^utm_/,
-        /^fbclid$/
-    ]
+	globDirectory: './',
+	globPatterns: [
+		'**/*.{png,js,html,conf,sh,css,json,md}'
+	],
+	swDest: 'sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
 };
